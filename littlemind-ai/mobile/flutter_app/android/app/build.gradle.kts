2907f7c2a986b5c0aa93ae4157b21da4f0e9cc81
jabhijeet <jabhijeet@gmail.com>
2026-05-15 15:52:14 +0800

    incremented version

--- littlemind-ai/mobile/flutter_app/android/app/build.gradle.kts
+++ littlemind-ai/mobile/flutter_app/android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 android {
     namespace = "io.github.jabhijeet.littlemindsai"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -32,7 +32,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = 34
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
